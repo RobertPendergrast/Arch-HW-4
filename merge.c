705d2caea31e629e4df850f20d17d62a8b629f6f
mthrndr <kristian.d.nikolov@gmail.com>
2025-12-06 21:37:23 -0500

    512 lvl 2 and 3

--- merge.c
+++ merge.c
@@ -23,7 +23,6 @@ void merge_128_registers(
     __m128i *right
 ) {
     // Reverse the *right register
-    *right = _mm_shuffle_epi32(*right, _128_REV);
 
     // Level 1
     // Get min/max values
@@ -66,6 +65,7 @@ void merge_128_registers(
 
 // 512 Constants
 const __mmask16 _512_BLEND_1 = 0b1111111100000000;
+const __mmask16 _512_BLEND_2 = 0b1111000011110000;
 
 /*
  * Takes in two m512i registers and merges them in place.
@@ -79,7 +79,6 @@ void merge_512_registers(
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
     );
     *right = _mm512_permutexvar_epi32(_512_REV, *right);
-    print_512_num(*right);
     // Level 1
     // Get min/max values
     __m512i L1 = _mm512_min_epi32(*left, *right);
@@ -91,20 +90,31 @@ void merge_512_registers(
         7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8
     );
     H1p = _mm512_permutexvar_epi32(_512_SHUFFLE_1, H1p);
-    print_512_num(L1p);
-    print_512_num(H1p);
     // Level 2
     // Get min/max values
+    __m512i L2 = _mm512_min_epi32(L1p, H1p);
+    __m512i H2 = _mm512_max_epi32(L1p, H1p);
     //Shuffle
+    __m512i L2p = _mm512_mask_blend_epi32(_512_BLEND_2, L2, H2);
+    __m512i H2p = _mm512_mask_blend_epi32(_512_BLEND_2, H2, L2);
+    __m512i _512_SHUFFLE_2 = _mm512_set_epi32(
+        11, 10, 9, 8, 15, 14, 13, 12, 3 , 2 , 1, 0, 7, 6, 5, 4
+    );
+    H1p = _mm512_permutexvar_epi32(_512_SHUFFLE_2, H1p);
     // Levels 3-4
     // Break each register up into 4 __mm128i registers and pass them into the
     // merge_128_registers function. Perhaps this should be put in an array...
-    __m128i H3_0 = _mm512_extracti32x4_epi32(*right, 0);
-    __m128i H3_1 = _mm512_extracti32x4_epi32(*right, 1);
-    __m128i H3_2 = _mm512_extracti32x4_epi32(*right, 2);
-    __m128i H3_3 = _mm512_extracti32x4_epi32(*right, 3);
-    __m128i L3_0 = _mm512_extracti32x4_epi32(*left, 0);
-    __m128i L3_1 = _mm512_extracti32x4_epi32(*left, 1);
-    __m128i L3_2 = _mm512_extracti32x4_epi32(*left, 2);
-    __m128i L3_3 = _mm512_extracti32x4_epi32(*left, 3);
+    __m128i H3_0 = _mm512_extracti32x4_epi32(H2p, 0);
+    __m128i H3_1 = _mm512_extracti32x4_epi32(H2p, 1);
+    __m128i H3_2 = _mm512_extracti32x4_epi32(H2p, 2);
+    __m128i H3_3 = _mm512_extracti32x4_epi32(H2p, 3);
+    __m128i L3_0 = _mm512_extracti32x4_epi32(L2p, 0);
+    __m128i L3_1 = _mm512_extracti32x4_epi32(L2p, 1);
+    __m128i L3_2 = _mm512_extracti32x4_epi32(L2p, 2);
+    __m128i L3_3 = _mm512_extracti32x4_epi32(L2p, 3);
+
+    merge_128_registers(&L3_0, &H3_0);
+    merge_128_registers(&L3_1, &H3_1);
+    merge_128_registers(&L3_2, &H3_2);
+    merge_128_registers(&L3_3, &H3_3);
 }
