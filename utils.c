cf9c18cee359d1147286d344957cc6a18c48ce06
Isaac Trost <wisaactrost@gmail.com>
2025-12-07 21:00:26 -0500

    sorting

--- utils.c
+++ utils.c
@@ -117,7 +117,7 @@ void print_512_num(__m512i var)
 
 // Order-independent hash using XOR and sum (parallel)
 // Returns two values: xor_hash and sum_hash
-static void compute_hash(uint32_t *arr, size_t size, uint64_t *xor_out, uint64_t *sum_out) {
+void compute_hash(uint32_t *arr, size_t size, uint64_t *xor_out, uint64_t *sum_out) {
     uint64_t xor_hash = 0;
     uint64_t sum_hash = 0;
     
